2e9e805f639c6291dc5cd71d8ef467e1dd0997aa
Amaranthe <Amaranthe@AmaranthedeMacBook-Air.local>
2015-07-03 01:06:08 +0800

    Initial Commit

---
+++ ks/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ks
+//
+//  Created by Amaranthe on 7/3/15.
+//  Copyright (c) 2015 Computers. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
